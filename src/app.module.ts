1f43e6b3decac072c09d3676a713f3e970f48535
VLaroye <laroye.vincent@gmail.com>
2023-01-19 10:04:19 +0100

    added configuration file handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
