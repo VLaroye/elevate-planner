a2d541e38bf4820f0feb919a4d5b001864b1e237
VLaroye <laroye.vincent@gmail.com>
2023-01-19 10:42:21 +0100

    initialized TypeOrm + Database Config + docker-compose to start postgres database + Created Task entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './tasks/task.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.username'),
+        entities: [Task],
+      }),
+      inject: [ConfigService],
     }),
     TasksModule,
   ],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,12 @@
+import * as process from 'process';
+
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3001,
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
+    database: process.env.DATABASE_DATABASE,
+  },
 });
